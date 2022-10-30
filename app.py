d6b4dd7ff947aee3a12f0ccbe1c11b2d860135b5
Bhanukiran97 <109714849+Bhanukiran97@users.noreply.github.com>
2022-10-30 18:39:40 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Oct 30 09:21:25 2022
+Created on Sun Oct 30 16:24:14 2022
 
 @author: BHANUKIRAN
 """
@@ -9,7 +9,7 @@ Created on Sun Oct 30 09:21:25 2022
 import uvicorn
 import pandas as pd
 import streamlit as st
-from datetime import date
+from datetime import date, timedelta
 import nsepy as ns
 from prophet import Prophet
 from prophet.plot import plot_plotly
@@ -18,27 +18,22 @@ from plotly import graph_objs as go
 
 
 
-now = date.today()
-
 st.title('Stock Forecast App')
 
 stocks = ('INFY', 'WIPRO', 'RELIANCE', 'TATAMOTORS')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
 
-n_years = st.slider('Years of prediction:', 1, 2)
-period = n_years * 365
-
+n_months = st.slider('months of prediction:', 1, 12)
+period = n_months * 30
 
 @st.cache
 def load_data(ticker):
-    data = ns.get_history(symbol = "INFY", start = date(2012,10,1), end = now)
+    data = ns.get_history(symbol = "INFY", start = date(2012,10,1), end = date.today())
     data.drop(['Symbol', 'Series', 'Prev Close', 'Last', 'VWAP', 'Turnover', 'Trades', 
                'Deliverable Volume','%Deliverble'],  axis = 1, inplace = True)
-    pd.DatetimeIndex(data.index, inplace = True)
     data.reset_index(inplace=True)
     return data
 
-	
 data_load_state = st.text('Loading data...')
 data = load_data(selected_stock)
 data_load_state.text('Loading data... done!')
@@ -47,11 +42,12 @@ st.subheader('Raw data')
 st.write(data.tail())
 
 # Plot raw data
+st.subheader('Time Series data with Rangeslider')
+
 def plot_raw_data():
 	fig = go.Figure()
-	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
 	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
-	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	fig.layout.update(xaxis_rangeslider_visible=True)
 	st.plotly_chart(fig)
 	
 plot_raw_data()
@@ -62,17 +58,21 @@ df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
 m = Prophet()
 m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
+future = m.make_future_dataframe(periods = period)
+future_df = future[-278:]
+forecast = m.predict(future_df)
+
+
+st.subheader(f'Forecast plot for {n_months} months')
+fig1 = plot_plotly(m, forecast, xlabel = 'Time', ylabel = 'Predictions')
+st.plotly_chart(fig1)
+
+forecast = forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']]
+forecast = forecast.rename(columns={"ds": "Date", "yhat": "Pred_Close", "yhat_lower" : "Pred_lower","yhat_upper":"Pred_upper"})
+forecast['Date'] = forecast['Date'].apply(lambda x: x.date())
+
 
 # Show and plot forecast
 st.subheader('Forecast data')
-st.write(forecast.head())
-    
-st.write(f'Forecast plot for {n_years} years')
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
+st.write(forecast.head(30))
 
-st.write("Forecast components")
-fig2 = m.plot_components(forecast)
-st.write(fig2)
